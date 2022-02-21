008d7a4e27c133ee82a08b49a92821299bb14cba
Hossammohamed9 <hossam.mohamed8008@gmail.com>
2022-02-21 22:18:31 +0200

    modify fragments names

--- app/src/main/java/com/example/gocart/ui/dashboard/CategoryFragment.kt
+++ app/src/main/java/com/example/gocart/ui/dashboard/CategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.gocart.ui.notifications
+package com.example.gocart.ui.dashboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.gocart.R
-import com.example.gocart.databinding.FragmentNotificationsBinding
+import com.example.gocart.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class CategoryFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var categoryViewModel: CategoryViewModel
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        categoryViewModel =
+            ViewModelProvider(this).get(CategoryViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textDashboard
+        categoryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
